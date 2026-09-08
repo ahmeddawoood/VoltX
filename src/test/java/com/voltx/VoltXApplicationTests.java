f11e74e175cddb3c5d32212ae33a1202c53a2e02
ahmeddawoood <163445492+ahmeddawoood@users.noreply.github.com>
2026-09-08 16:42:21 +0300

    test(build): add Spring context smoke test

---
+++ src/test/java/com/voltx/VoltXApplicationTests.java
@@ -0,0 +1,12 @@
+package com.voltx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoltXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
